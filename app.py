40fc851435d8e4031421144a94887dfa8a7cb738
AliAhmadAyoob <aliahmadayoob@gmail.com>
2025-12-30 21:55:20 +0500

    Refactor app.py to comment out model loading
    Commented out the model loading and image display sections, and fixed a typo in the image conversion method.

--- app.py
+++ app.py
@@ -4,23 +4,25 @@ from PIL import Image
 
 @st.cache_resource
 def load_model():
-    return YOLO('helmet-model.pt')
+    # return YOLO('helmet-model.pt')
+    pass
 model = load_model()
 
-st.set_page_config(page_title="Helmet Detection Model", page_icon="🛰️")
+st.set_page_config(page_title="Solar Plate Detection Model", page_icon="🛰️")
 
 st.title("🛰️ Solar Plate Detection")
-st.write("Upload image of solar panel to detect whether it is clear/not clear.")
+st.write("Upload image of solar panel to detect whether it is **clear/not clear**.")
 uploaded = st.file_uploader('Choose an image...',type=['jpg','jpeg','png'])
 
 if uploaded is not None:
     if uploaded.type.startswith('image'):
-        img = Image.open(uploaded).convet("RGB")
-        st.image(img,caption='uploaded image',use_column_width=True)
+        img = Image.open(uploaded).convert("RGB")
+        # st.image(img,caption='uploaded image',use_column_width=True)
 
-        with spinner('Detecting....'):
+        with st.spinner('Detecting....'):
             pred = model.predict(img)
             result_img = pred[0].plot()[ :,:,::-1]
             st.image(result_img,caption='Detected image',use_container_width=True)
 
         
+
